0ad6e3a926bc0d2d2c7b72171ad4f9fc8a6d15f9
KorotkovaDaria <qw585911@mail.ru>
2019-01-10 21:44:56 +0400

    "Add import xpath"

--- src/main/java/locators/AuthorizationPageLocators.java
+++ src/main/java/locators/AuthorizationPageLocators.java
@@ -1,10 +1,13 @@
 package locators;
 
 import org.openqa.selenium.By;
+
+import static org.openqa.selenium.By.xpath;
+
 /**Локаторы для страницы авторизации*/
 public class AuthorizationPageLocators {
-    public static final By LOGIN_FIELD = By.xpath("//*[@id='identifierId']");
-    public static final By LOGIN_NEXT_BUTTON = By.xpath("//*[@id='identifierNext']");
-    public static final By PASSWORD_NEXT_BUTTON = By.xpath("//span[contains(text(),'Далее')]");
-    public static final By PASSWORD_FIELD = By.xpath("//input[@name='password']");
+    public static final By LOGIN_FIELD = xpath("//*[@id='identifierId']");
+    public static final By LOGIN_NEXT_BUTTON = xpath("//*[@id='identifierNext']");
+    public static final By PASSWORD_NEXT_BUTTON = xpath("//span[contains(text(),'Далее')]");
+    public static final By PASSWORD_FIELD = xpath("//input[@name='password']");
 }
