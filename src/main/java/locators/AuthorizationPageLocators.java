7de3a0d7918a8ce1d4dd28f3863d3b252c320575
KorotkovaDaria <qw585911@mail.ru>
2019-01-10 21:44:00 +0400

    "Add comment into autopageloc"

--- src/main/java/locators/AuthorizationPageLocators.java
+++ src/main/java/locators/AuthorizationPageLocators.java
@@ -1,7 +1,7 @@
 package locators;
 
 import org.openqa.selenium.By;
-
+/**Локаторы для страницы авторизации*/
 public class AuthorizationPageLocators {
     public static final By LOGIN_FIELD = By.xpath("//*[@id='identifierId']");
     public static final By LOGIN_NEXT_BUTTON = By.xpath("//*[@id='identifierNext']");
